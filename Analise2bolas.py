68ff8d95ca53314601b217f5db109e1fcb88ef1a
hfmenhem <75636969+hfmenhem@users.noreply.github.com>
2024-08-16 11:46:29 -0300

    Código para analisar bolas 2

--- Analise2bolas.py
+++ Analise2bolas.py
@@ -11,9 +11,9 @@ def seno(x, A, B, fase, C):
 
 SO = levitacao.SimuladorOndas(uL='m', uM='kg', uT='s') 
 
-z0 = 17e-3
+z0 = 12e-3
 
-zm = 1e-3*np.linspace(4,8.5, 100)
+zm = 1e-3*np.linspace(4,10, 100)
 coord=[]
 for zi in zm:
     coord.append(np.array([0,0,zi]))
@@ -32,12 +32,14 @@ for zi in zm0:
 SO.criaEmissor(7e-3, 1e-3, [0,0,1], [0,0,-1e-3], 1)
 SO.criaEmissor(7e-3, 1e-3, [0,0,-1], [0,0,21e-3], 1, fase = 0)
 #SO.criaRefletor(7e-3, 1e-3, [0,0,-1], [0,0,21e-3])
-SO.criaBola(.5e-3, 10, [0,0,z0])
+SO.criaBola(1e-3, 20, [0,0,z0])
 
 #medir tempo
 start = timeit.default_timer()
 
 F0, G0 = SO.calculaForca(coord0, 6,CalGorcov=True, Bolas=False)
+
+F1, G1 = SO.calculaForca(coord, 6,CalGorcov=True, Bolas=False)
 
 F, G = SO.calculaPar2Bola(coord, coordB, 6, CalGorcov=True)
 
@@ -47,9 +49,10 @@ execution_time = stop - start
 print("Programa demorou "+str(execution_time)+" s para calcular")
 
 
+
 parametros =[]
 #bounds0 = [[0, 0, 0], [np.inf, 2*np.pi/20e-3, 2*np.pi]]
-for Fi in F:
+for Fi in [*F, F1]:
     Amp = (np.max(Fi[:,2])-np.min(Fi[:,2]))/2
  
     B = abs(np.pi/(zm[np.argmax(Fi[:,2])]-zm[np.argmin(Fi[:,2])]))
@@ -68,6 +71,11 @@ for Pari in parametros:
 equilibrio = np.array(equilibrio)
 grad = np.array(grad)
 
+equilibrio0 = equilibrio[-1]
+grad0 = grad[-1]
+
+equilibrio = equilibrio[:-1]
+grad = grad[:-1]
 
 plt.figure(dpi =300)
 plt.plot(1e3*zm0, 1e-3*F0[:,2], marker='', label = "sem bola", linestyle ='-')
@@ -84,6 +92,7 @@ plt.subplot(2,1,1)
 
 plt.title("Com 1 bola variando")
 plt.plot(1e3*(bm+z0), 1e3*equilibrio, marker='.', linestyle ='-')
+plt.plot([1e3*(bm[1]+z0), 1e3*(bm[-1]+z0)], [1e3*equilibrio0, 1e3*equilibrio0], marker='', linestyle ='-')
 
 plt.ylabel( 'Posição de equilibrio \n (mm)')
 plt.grid()
@@ -93,6 +102,7 @@ plt.subplots_adjust(hspace=0.4)
 plt.subplot(2,1,2)
 
 plt.plot(1e3*(bm+z0), 1e-6*grad, marker='.', linestyle ='-')
+plt.plot([1e3*(bm[1]+z0), 1e3*(bm[-1]+z0)], [1e-6*grad0, 1e-6*grad0], marker='', linestyle ='-')
 
 plt.xlabel( 'Posição da Bola 2 (mm)' )
 plt.ylabel( 'Gradiente Força \n (kN/m^3/nm)')
---
+++ Analise2bolas2.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import levitacao
+from scipy.optimize import curve_fit
+
+import timeit
+
+D = 20e-3
+disc = 1e-3
+R = 7e-3
+Zb = 12e-3
+
+SO = levitacao.SimuladorOndas(uL='m', uM='kg', uT='s') 
+
+SO.criaEmissor(R, disc, [0,0,1], [0,0,0], 1)
+SO.criaEmissor(R, disc, [0,0,-1], [0,0,D], 1, fase = np.pi)
+#SO.criaRefletor(7e-3, 1e-3, [0,0,-1], [0,0,21e-3])
+
+SO.criaBola(1e-3, 20, [0,0,Zb])
+
+zT = np.linspace(disc, D-disc, 100)
+coordT = np.zeros([len(zT), 3])
+coordT[:,2] = zT
+
+F0, G0 = SO.calculaForca(coordT, 6,CalGorcov=True, Bolas=False)
+F0z = F0[:,2]
+
+#Acha pt equilibrio
+zZeros = zT
+FZeros = F0z
+for i in range(0,2):
+    zteste = zZeros[:-1]
+    multTeste = FZeros[1:]*FZeros[:-1]
+    difTeste = FZeros[1:]-FZeros[:-1]
+    cond0 = np.logical_and(multTeste<0, difTeste<0 )
+    int0 = np.transpose(((zteste[:-1])[cond0[:-1]],  (zteste[1:])[cond0[:-1]]))
+    zZeros =np.ndarray.flatten(np.transpose(np.linspace(int0[:,0], int0[:,1])))  
+    coordzeros = np.zeros([len(zZeros), 3])
+    coordzeros[:,2] =zZeros
+    FZeros, GZeros = SO.calculaForca(coordzeros, 6,CalGorcov=True, Bolas=False)
+    FZeros=FZeros[:,2]
+zteste = zZeros[:-1]
+multTeste = FZeros[1:]*FZeros[:-1]
+difTeste = FZeros[1:]-FZeros[:-1]
+cond0 = np.logical_and(multTeste<0, difTeste<0 )
+int0 = np.transpose(((zteste[:-1])[cond0[:-1]],  (zteste[1:])[cond0[:-1]]))
+Fint0 = np.transpose(((FZeros[:-2])[cond0[:-1]],  (FZeros[1:-1])[cond0[:-1]]))
+
+zeros = (int0[:,0]*Fint0[:,1]-int0[:,1]*Fint0[:,0])/(Fint0[:,1]-Fint0[:,0])
+grad = (Fint0[:,1]-Fint0[:,0])/(int0[:,1]-int0[:,0])
+
+#gráfico Total
+
+plt.figure(dpi =300)
+plt.plot(1e3*zT, 1e-3*F0z, marker='', label = "sem bola", linestyle ='-')
+z0s = np.ndarray.flatten(int0)
+f0s = np.ndarray.flatten(Fint0)
+plt.plot(1e3*z0s, 1e-3*f0s, marker='.', label = "sem bola", linestyle ='')
+plt.plot(1e3*zeros, np.zeros(len(zeros)), marker='.', label = "sem bola", linestyle ='')
+#plt.plot(1e3*zZeros, 1e-3*FZeros, marker='.', label = "sem bola", linestyle ='')
+
+plt.xlim(0, D*1e3)
+plt.xlabel( 'Posição (mm)' )
+plt.ylabel( 'Força (kN/m^3)')
+plt.title("Sem bola")
+plt.grid()
+plt.show()
